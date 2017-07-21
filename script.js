718f8646a1427da79ac7d7a3f1051f89e1d852be
marquasiahayes <hayes.marquasia@yahoo.com>
2017-07-21 15:42:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function ubbi(){
+  var hello ="hello"
+  var ubb = "ubb"
+  var txt= "";
+  var i =0
+  while (i<5){
+
+    var rs = hello.charAt(i);
+    i=i+1
+    if (res=="a" res=="e" res=="i" res=="o" res=="u")
+    txt=txt+"ubb"+res
+  }
+  else {
+    txt=txt+res
+  }
+}
+document.getElementById("demo")..innerHTML= txt;
